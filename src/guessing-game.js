eb33f385c907deec238a21f9bc682942f8c00dc6
malek2005 <malek2005@list.ru>
2021-08-30 14:38:41 +0300

    feat: solution for task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min;
+        this.max;
+        this.guess_res
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.guess_res = Math.ceil((this.min + this.max) / 2);
+        return this.guess_res;
     }
 
     lower() {
-
+        this.max = this.guess_res;
     }
 
     greater() {
-
+        this.min = this.guess_res;
     }
 }
 
